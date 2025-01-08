8bd1bde64f8cc49aa36ba3c49a2290b14dfe6fd6
parth20-10 <Parth.Masal@wissen.com>
2025-01-08 16:35:28 +0530

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,12 @@
+ package application;
+
+public class Manager extends Employee {
+	public Manager(String name,int age) {
+		setName(name);
+		setAge(age);
+		setDesignation("Manager");
+		setSalary(75000);
+	}
+	
+
+}
---
+++ Programmer.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Programmer extends Employee{
+	
+	public Programmer(String name,int age) {
+		setName(name);
+		setAge(age);
+		setDesignation("Programmer");
+		setSalary(50000);
+	}
+	
+
+}
